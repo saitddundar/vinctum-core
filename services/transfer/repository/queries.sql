16418d17a945091799a62a9b8c4fe0ceb5fd6d3e
Sait Dundar <saitdndr51@gmail.com>
2026-03-27 03:09:56 +0300

    feat(transfer): add sqlc queries, config and generated repository

---
+++ services/transfer/repository/queries.sql
@@ -0,0 +1,22 @@
+-- name: CreateTransfer :one
+INSERT INTO transfers (transfer_id, sender_node_id, receiver_node_id, filename, total_size_bytes, content_hash, chunk_size_bytes, total_chunks, status)
+VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+RETURNING *;
+
+-- name: GetTransfer :one
+SELECT * FROM transfers WHERE transfer_id = $1;
+
+-- name: ListTransfersByNode :many
+SELECT * FROM transfers WHERE sender_node_id = $1 OR receiver_node_id = $1 ORDER BY created_at DESC;
+
+-- name: ListTransfersByStatus :many
+SELECT * FROM transfers WHERE (sender_node_id = $1 OR receiver_node_id = $1) AND status = $2 ORDER BY created_at DESC;
+
+-- name: UpdateTransferProgress :exec
+UPDATE transfers SET chunks_done = $2, updated_at = NOW() WHERE transfer_id = $1;
+
+-- name: UpdateTransferStatus :exec
+UPDATE transfers SET status = $2, updated_at = NOW() WHERE transfer_id = $1;
+
+-- name: CompleteTransfer :exec
+UPDATE transfers SET status = 3, chunks_done = total_chunks, updated_at = NOW() WHERE transfer_id = $1;
