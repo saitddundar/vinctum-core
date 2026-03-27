7abfc72c4a2aa18c46f2c8310022e8366ab3d20a
Sait Dundar <saitdndr51@gmail.com>
2026-03-27 03:09:45 +0300

    feat(routing): add service entry point and Dockerfile

---
+++ cmd/routing/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"context"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/rs/zerolog/log"
+	"github.com/saitddundar/vinctum-core/internal/migrator"
+	"github.com/saitddundar/vinctum-core/pkg/config"
+	"github.com/saitddundar/vinctum-core/pkg/logger"
+	"github.com/saitddundar/vinctum-core/pkg/middleware"
+	routingv1 "github.com/saitddundar/vinctum-core/proto/routing/v1"
+	migrations "github.com/saitddundar/vinctum-core/scripts/migrations"
+	routinghandler "github.com/saitddundar/vinctum-core/services/routing/handler"
+	"github.com/saitddundar/vinctum-core/services/routing/repository"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+func main() {
+	cfg, err := config.Load("")
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to load config")
+	}
+
+	logger.Init(cfg.Service.Name, cfg.Service.Version, cfg.Service.LogLevel, cfg.Service.Environment == "development")
+
+	ctx := context.Background()
+
+	pool, err := pgxpool.New(ctx, cfg.Database.DSN)
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to connect to postgres")
+	}
+	defer pool.Close()
+
+	if err := pool.Ping(ctx); err != nil {
+		log.Fatal().Err(err).Msg("postgres ping failed")
+	}
+
+	if err := migrator.Run(ctx, pool, migrations.FS); err != nil {
+		log.Fatal().Err(err).Msg("migration failed")
+	}
+
+	queries := repository.New(pool)
+	handler := routinghandler.NewRoutingHandler(queries)
+
+	lis, err := net.Listen("tcp", cfg.GRPC.Address())
+	if err != nil {
+		log.Fatal().Err(err).Str("addr", cfg.GRPC.Address()).Msg("failed to listen")
+	}
+
+	srv := grpc.NewServer(
+		grpc.UnaryInterceptor(middleware.UnaryAuthInterceptor(cfg.Auth.JWTSecret)),
+		grpc.StreamInterceptor(middleware.StreamAuthInterceptor(cfg.Auth.JWTSecret)),
+	)
+
+	routingv1.RegisterRoutingServiceServer(srv, handler)
+	reflection.Register(srv)
+
+	log.Info().Str("addr", cfg.GRPC.Address()).Msg("routing service starting")
+
+	go func() {
+		if err := srv.Serve(lis); err != nil {
+			log.Fatal().Err(err).Msg("server error")
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	log.Info().Msg("shutting down routing service")
+	srv.GracefulStop()
+}
