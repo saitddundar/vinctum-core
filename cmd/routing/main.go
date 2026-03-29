8e23876e0db849d49fdd1d373c9cb0c51661f13f
Sait Dundar <saitdndr51@gmail.com>
2026-03-29 17:17:15 +0300

    feat(routing): auto-populate route table from discovery peer updates

--- cmd/routing/main.go
+++ cmd/routing/main.go
@@ -7,17 +7,21 @@ import (
 	"os/signal"
 	"syscall"
 
+	"io"
+
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/rs/zerolog/log"
 	"github.com/saitddundar/vinctum-core/internal/migrator"
 	"github.com/saitddundar/vinctum-core/pkg/config"
 	"github.com/saitddundar/vinctum-core/pkg/logger"
 	"github.com/saitddundar/vinctum-core/pkg/middleware"
+	discoveryv1 "github.com/saitddundar/vinctum-core/proto/discovery/v1"
 	routingv1 "github.com/saitddundar/vinctum-core/proto/routing/v1"
 	migrations "github.com/saitddundar/vinctum-core/scripts/migrations"
 	routinghandler "github.com/saitddundar/vinctum-core/services/routing/handler"
 	"github.com/saitddundar/vinctum-core/services/routing/repository"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/reflection"
 )
 
@@ -68,6 +72,28 @@ func main() {
 	routingv1.RegisterRoutingServiceServer(srv, handler)
 	reflection.Register(srv)
 
+	// Subscribe to discovery peer updates to auto-populate routing table.
+	discoveryAddr := os.Getenv("VINCTUM_DISCOVERY_ADDR")
+	if discoveryAddr == "" {
+		discoveryAddr = "localhost:50052"
+	}
+
+	nodeID := os.Getenv("VINCTUM_NODE_ID")
+	if nodeID == "" {
+		nodeID = cfg.Service.Name
+	}
+
+	discoveryConn, err := grpc.NewClient(discoveryAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Warn().Err(err).Msg("could not dial discovery service, auto-routing disabled")
+	} else {
+		dc := discoveryv1.NewDiscoveryServiceClient(discoveryConn)
+		defer discoveryConn.Close()
+
+		go watchPeerUpdates(ctx, dc, handler, nodeID)
+		log.Info().Str("addr", discoveryAddr).Msg("subscribed to discovery peer updates")
+	}
+
 	log.Info().Str("addr", cfg.GRPC.Address()).Msg("routing service starting")
 
 	go func() {
@@ -82,4 +108,47 @@ func main() {
 
 	log.Info().Msg("shutting down routing service")
 	srv.GracefulStop()
+}
+
+func watchPeerUpdates(ctx context.Context, dc discoveryv1.DiscoveryServiceClient, handler *routinghandler.RoutingHandler, nodeID string) {
+	stream, err := dc.StreamPeerUpdates(ctx, &discoveryv1.StreamPeerUpdatesRequest{NodeId: nodeID})
+	if err != nil {
+		log.Warn().Err(err).Msg("failed to open peer updates stream")
+		return
+	}
+
+	for {
+		update, err := stream.Recv()
+		if err != nil {
+			if err == io.EOF {
+				log.Info().Msg("peer updates stream closed")
+				return
+			}
+			log.Warn().Err(err).Msg("peer updates stream error")
+			return
+		}
+
+		if update.Peer == nil {
+			continue
+		}
+
+		peer := update.Peer
+		switch update.Type {
+		case discoveryv1.PeerUpdate_PEER_JOINED, discoveryv1.PeerUpdate_PEER_UPDATED:
+			// Add a direct route entry (metric=1) for the new/updated peer.
+			_, _ = handler.UpdateRouteTable(ctx, &routingv1.UpdateRouteTableRequest{
+				NodeId: nodeID,
+				Entries: []*routingv1.RouteEntry{{
+					TargetNodeId: peer.NodeId,
+					NextHopId:    peer.NodeId,
+					Metric:       1,
+					LatencyMs:    0,
+				}},
+			})
+			log.Debug().Str("peer", peer.NodeId).Msg("route added from peer update")
+
+		case discoveryv1.PeerUpdate_PEER_LEFT:
+			log.Debug().Str("peer", peer.NodeId).Msg("peer left, route may need cleanup")
+		}
+	}
 }
